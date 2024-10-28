27ff67b5d05e6fbba0fd7b6b722f17f577e75d2f
Ícar N. S. <icar.nin@protonmail.com>
2024-10-28 14:09:06 +0100

    non empty queviure nom

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,12 +1,18 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text, check } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
-export const queviuresTable = sqliteTable("queviures", {
-  nom: text("nom").primaryKey(),
-  dataCreacio: integer("data_creacio", { mode: "timestamp_ms" })
-    .notNull()
-    .default(sql`(unixepoch() * 1000)`),
-});
+export const queviuresTable = sqliteTable(
+  "queviures",
+  {
+    nom: text("nom").primaryKey(),
+    dataCreacio: integer("data_creacio", { mode: "timestamp_ms" })
+      .notNull()
+      .default(sql`(unixepoch() * 1000)`),
+  },
+  (table) => ({
+    nonEmptyNom: check("nom_non_empty_check", sql`len(${table.nom}) > 0`),
+  }),
+);
 
 export type QueviuresSelect = typeof queviuresTable.$inferSelect;
 export type QueviuresInsert = typeof queviuresTable.$inferInsert;
