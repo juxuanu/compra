d55f61e9425528b1a50356b9453be2610ecb98c3
Ícar Nin Solana <icar.nin@protonmail.com>
2024-11-21 00:22:18 +0100

    unused import

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,4 +1,4 @@
-import { integer, sqliteTable, text, check } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 export const queviuresTable = sqliteTable("queviures", {
